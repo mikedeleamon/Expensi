5ece091e912ed5722ec8a11b976cb5696d5a2381
Dele Amon <deleamon@deles-mbp.myfiosgateway.com>
2022-11-21 13:16:24 -0500

    setup webpack prod build

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,13 @@
 
 //load in a node module:
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports={
+module.exports =(env) => {
+const isProduction = env === 'production'
+const CSSExtract = new ExtractTextPlugin('styles.css')
+
+    return{
     entry: './src/app.js',
     output: {
         //output path must be explicitly declared. use __dirname to access dir location
@@ -28,21 +33,36 @@ module.exports={
             test:/\.s?css$/,
             //use allows you to load multiple loaders at once
             //PUT THE STYLE LOADER BEFORE THE CSS LOADER!!!!!!
-            use:['style-loader',
-                'css-loader',
-                'sass-loader']
+            use:CSSExtract.extract({
+               use: [
+                {loader:'css-loader',
+                options:{
+                    sourceMap:true
+                }},
+                {loader:'sass-loader',
+                options:{
+                    sourceMap:true
+                }}
+                ]
+            })
         }
     ]
     },
+
+    plugins: [
+        CSSExtract
+    ],
    
     //cheap-module-eval-source-map allows you to locate errors where you origionally wrote them
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction? 'source-map' : 'inline-source-map',
    
     //development server for the app
     devServer:{
         contentBase: [path.join(__dirname,'public')],
         //this tells app that this app is using client side routing when the value is true
         historyApiFallback:true
-    }
+    }}
 }
+
+
 
