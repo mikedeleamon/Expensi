cf35033869bea24076c6511c34b45534704782f0
Dele Amon <deleamon@deles-mbp.myfiosgateway.com>
2022-11-27 20:17:04 -0500

    set up test DB env

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const webpack = require('webpack')
+
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'developement'
 
 if (process.env.NODE_ENV=='test'){
     require('dotenv').config({path: '.env.test'})
-} else if(process.env.NODE_ENV == development){
+} else if(process.env.NODE_ENV == 'development'){
     require('dotenv').config({path: '.env.developement'})
 }
 
